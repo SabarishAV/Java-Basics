4c3052880937fa34e228118a6264867d90b4aa58
Sabarish A V <avsabarish@gmail.com>
2025-07-03 23:32:03 +0530

    generics

---
+++ Generics/Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String args[]){
+        Pair<String, Integer> student = new Pair<>("John",1);
+        Pair<Integer, Boolean> productStock = new Pair<>(101, true);
+        System.out.println(student);
+        System.out.println(productStock);
+    }
+}
